d23b189ba345207545de4644ad174d315189d853
Andrei Nicolae <andreicalutiu@gmail.com>
2024-12-09 21:44:19 +0200

    first component migrated to module

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+
+import { AppComponent } from './app.component'
+import { HeaderComponent } from './header/header.component'
+import { UserComponent } from './user/user.component'
+import { TaskComponent } from './tasks/task/task.component'
+
+@NgModule({
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, HeaderComponent, UserComponent, TaskComponent]
+})
+export class AppModule {}
