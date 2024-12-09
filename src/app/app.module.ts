cc7fd005d24b356960393239570fd29a8e4fe63d
Andrei Nicolae <andreicalutiu@gmail.com>
2024-12-09 22:04:27 +0200

    final switch to modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppComponent } from './app.component'
 import { HeaderComponent } from './header/header.component'
 import { UserComponent } from './user/user.component'
-import { TaskComponent } from './tasks/task/task.component'
+import { SharedModule } from './shared/shared.module'
+import { TasksModule } from './tasks/tasks.module'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    UserComponent,
+    HeaderComponent],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, HeaderComponent, UserComponent, TaskComponent]
+  imports: [BrowserModule, SharedModule, TasksModule]
 })
 export class AppModule {}
